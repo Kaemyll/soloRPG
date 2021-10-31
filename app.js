72d6653d1902a3411b85a847e18a284bbc172dc3
Garnier David <david.garnier483@orange.fr>
2021-10-31 16:28:10 +0100

    bug: addEventListener method not working with oracle

--- app.js
+++ app.js
@@ -6,45 +6,7 @@ var dice = {
 		return randomNumber;
 	}
 };
-
-function printNumber(number, text) {
-	var diceResult = document.getElementById('diceResult');
-	diceResult.innerHTML = (number, text);
-}
-
-var button = document.getElementById('destiny');
-console.log('button est défini = ', button);
-
-button.onclick = function() {
-	var result = dice.roll();
-	console.log('result = ', result);
-	switch (result) {
-		case 1:
-			var resultText = 'Non, et...';
-			console.log('dice = ', result);
-			break;
-		case 2:
-		case 3:
-			var resultText = 'Non, mais...';
-			console.log('dice = ', result);
-			break;
-		case 4:
-		case 5:
-			var resultText = 'Oui, mais...';
-			console.log('dice = ', result);
-			break;
-		case 6:
-			var resultText = 'Oui, et...';
-			console.log('dice = ', result);
-			break;
-		default:
-			var resultText = 'Résultat...';
-			console.log('dice = ', result);
-	}
-	printNumber(result, resultText);
-};
-
-// Tirage de carte de l'ORACLE
+console.log('Jet de dé à sec = ', dice.roll()); // TEST
 var tarot = {
 	sides  : 78,
 	tirage : function() {
@@ -52,40 +14,69 @@ var tarot = {
 		return randomCard;
 	}
 };
+console.log('tirage de carte à sec = ', tarot.tirage()); // TEST
 
+var button = document.getElementById('destiny');
+console.log('button est défini = ', button); // TEST
+var buttonCard = document.getElementById('oracle');
+console.log('buttonCard est défini = ', buttonCard); // TEST
+
+function printNumber(number, text) {
+	var diceResult = document.getElementById('diceResult');
+	diceResult.innerHTML = (number, text);
+}
 function printCard(card, textCard) {
 	var cardResult = document.getElementById('cardResult');
 	cardResult.innerHTML = (card, textCard);
 }
 
-var buttonCard = document.getElementById('oracle');
-console.log('buttonCard est défini = ', buttonCard);
-
-buttonCard.onclick = function() {
-	var result = tarot.tirage();
-	console.log(result);
+button.addEventListener('click', function() {
+	var result = dice.roll();
+	console.log('result du button.onclick = ', result); // TEST
 	switch (result) {
 		case 1:
 			var resultText = 'Non, et...';
-			console.log('dice = ', result);
 			break;
 		case 2:
 		case 3:
 			var resultText = 'Non, mais...';
-			console.log('dice = ', result);
 			break;
 		case 4:
 		case 5:
 			var resultText = 'Oui, mais...';
-			console.log('dice = ', result);
 			break;
 		case 6:
 			var resultText = 'Oui, et...';
-			console.log('dice = ', result);
 			break;
 		default:
 			var resultText = 'Résultat...';
-			console.log('dice = ', result);
 	}
-	printCard(result, resultText);
-};
+	printNumber(result, resultText);
+});
+
+// Tirage de carte de l'ORACLE
+
+buttonCard.addEventListener('click', function() {
+	console.log('Test du click sur Oracle');
+	// var result = tarot.tirage();
+	// console.log(result);
+	// switch (result) {
+	// 	case 1:
+	// 		var resultText = 'Non, et...';
+	// 		break;
+	// 	case 2:
+	// 	case 3:
+	// 		var resultText = 'Non, mais...';
+	// 		break;
+	// 	case 4:
+	// 	case 5:
+	// 		var resultText = 'Oui, mais...';
+	// 		break;
+	// 	case 6:
+	// 		var resultText = 'Oui, et...';
+	// 		break;
+	// 	default:
+	// 		var resultText = 'Résultat...';
+	// }
+	// printCard(result, resultText);
+});
