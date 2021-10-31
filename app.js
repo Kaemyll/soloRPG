1d76083dea4a93a080a36ca87dbadcabb8e6d3e6
Garnier David <david.garnier483@orange.fr>
2021-10-31 14:37:27 +0100

    bug: button.onclick = null

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ function printNumber(number, text) {
 }
 
 var button = document.getElementById('destiny');
+console.log('button est défini = ', button);
 
 button.onclick = function() {
 	var result = dice.roll();
+	console.log('result = ', result);
 	switch (result) {
 		case 1:
 			var resultText = 'Non, et...';
@@ -40,4 +42,50 @@ button.onclick = function() {
 			console.log('dice = ', result);
 	}
 	printNumber(result, resultText);
+};
+
+// Tirage de carte de l'ORACLE
+var tarot = {
+	sides  : 78,
+	tirage : function() {
+		var randomCard = Math.floor(Math.random() * this.sides) + 1;
+		return randomCard;
+	}
+};
+
+function printCard(card, textCard) {
+	var cardResult = document.getElementById('cardResult');
+	cardResult.innerHTML = (card, textCard);
+}
+
+var buttonCard = document.getElementById('oracle');
+console.log('buttonCard est défini = ', buttonCard);
+
+buttonCard.onclick = function() {
+	var result = tarot.tirage();
+	console.log(result);
+	switch (result) {
+		case 1:
+			var resultText = 'Non, et...';
+			console.log('dice = ', result);
+			break;
+		case 2:
+		case 3:
+			var resultText = 'Non, mais...';
+			console.log('dice = ', result);
+			break;
+		case 4:
+		case 5:
+			var resultText = 'Oui, mais...';
+			console.log('dice = ', result);
+			break;
+		case 6:
+			var resultText = 'Oui, et...';
+			console.log('dice = ', result);
+			break;
+		default:
+			var resultText = 'Résultat...';
+			console.log('dice = ', result);
+	}
+	printCard(result, resultText);
 };
